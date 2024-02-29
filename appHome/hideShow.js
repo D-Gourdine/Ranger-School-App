a03d2fffd90b460435568969c960f2402c928ded
D'Angelo Gourdine <47831423+D-Gourdine@users.noreply.github.com>
2024-02-29 15:23:24 -0500

    added pdfs and button hide show js

---
+++ appHome/hideShow.js
@@ -0,0 +1,8 @@
+function myFunction(input) {
+    var x = document.getElementById(input);
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
