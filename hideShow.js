e096340d338a408213ea689accfefdfefcb6da03
Keno Deary Jr <kenodeary3@gmail.com>
2024-03-20 16:21:03 -0400

    updated header

--- hideShow.js
+++ hideShow.js
@@ -8,7 +8,7 @@ function myFunction(input) {
   }
 
 // When the user scrolls the page, execute myFunction 
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {myFunction(); growShrinkLogo()};
 
 function myFunction() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
@@ -17,3 +17,12 @@ function myFunction() {
   document.getElementById("myBar").style.width = scrolled + "%";
 }
 
+
+function growShrinkLogo() {
+  var Logo = document.getElementById("Logo")
+  if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
+    Logo.style.width = "150px";
+  } else {
+    Logo.style.width = "300px";
+  }
+}
